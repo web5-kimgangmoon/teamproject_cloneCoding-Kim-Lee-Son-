891d44c6e23d31e94f668942ea899555e0d33dbe
kimgangmoon <rkdans2111@naver.com>
2024-05-20 11:38:47 +0900

    feat:structureRevise

--- backEnd/controllers/test/index.js
+++ backEnd/controllers/test/index.js
@@ -1,13 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-<<<<<<< HEAD
 import test from "../../services/test.js";
-=======
-router.get("/", (req, res) => {
-  res.send("test");
-});
->>>>>>> 9719c15 (feat:path '/test' delete async)
 
 router.use("/", test);
 
