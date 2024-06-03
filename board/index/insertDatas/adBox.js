5d8dcb0bfa6974e633610a3fe41fc4b4413ac834
kimgangmoon <rkdans2111@naver.com>
2024-06-03 14:53:18 +0900

    feat:lastMaybe

--- board/index/insertDatas/adBox.js
+++ board/index/insertDatas/adBox.js
@@ -4,8 +4,9 @@
 <div class="adImg"><img src="./../imgs/img2.gif" /></div>
 </a> */}
 
-document.getElementById("upDownSlide") =
-    `<div class="upDownSlide out" id="upDownSlide">
+if (document.getElementById("upDownSlide")) {
+    document.getElementById("upDownSlide").innerHTML =
+        `
 <div class="divideAd adOne">
   <div class="adImgBox">
     <a href="/">
@@ -15,58 +16,58 @@ document.getElementById("upDownSlide") =
       <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
   </div>
 </div>
 <div class="divideAd adTwo">
   <div class="adImgBox">
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
   </div>
 </div>
 <div class="divideAd adThree">
   <div class="adImgBox">
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
   </div>
 </div>
 <div class="divideAd adFour">
   <div class="adImgBox">
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
   </div>
 </div>
@@ -89,93 +90,111 @@ document.getElementById("upDownSlide") =
 <div class="divideAd adSix">
   <div class="adImgBox">
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
   </div>
 </div>
 <div class="divideAd adSeven">
   <div class="adImgBox">
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
   </div>
 </div>
 <div class="divideAd adEight">
   <div class="adImgBox">
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
   </div>
 </div>
 <div class="divideAd adNine">
   <div class="adImgBox">
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
   </div>
 </div>
 <div class="divideAd adTen">
   <div class="adImgBox">
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/notification-bell.png" /></div>
+      <div class="adImg"><img src="./../imgs/maxresdefault.jpg" /></div>
     </a>
     <a href="/">
-      <div class="adImg"><img src="./../imgs/person.png" /></div>
+      <div class="adImg"><img src="./../imgs/img2.gif" /></div>
     </a>
   </div>
-</div>
-</div>
+</div>`
+}
+
+if (document.getElementById("basicSlide")) {
+    document.getElementById("basicSlide").innerHTML =
+        `
 <div class="basicSlide" id="basicSlide">
 <div class="adImgBox">
-  <a href="/"><img src="./../imgs/notification-bell.png" /></a>
-  <a href="/"><img src="./../imgs/person.png" /></a>
-  <a href="/"><img src="./../imgs/notification-bell.png" /></a>
+  <a href="/"><img src="./../imgs/maxresdefault.jpg" /></a>
+  <a href="/"><img src="./../imgs/img2.gif" /></a>
+  <a href="/"><img src="./../imgs/maxresdefault.jpg" /></a>
 </div>
-</div>`
+</div>`;
+}
 
+if (document.getElementById("left_ad")) {
+    document.getElementById("left_ad").innerHTML =
+        `
+<a href="/"><img src="./../imgs/maxresdefault.jpg" /></a>
+`;
+}
 
-        `<div class="left_ad" id="left_ad">
-<a href="/"><img src="./../imgs/notification-bell.png" /></a>
-</div>`
+if (document.getElementById("adBoxCenter")) {
+    document.getElementById("adBoxCenter").innerHTML = `
+
+<a href="/">
+  <div class="imgWrapper">
+    <img src="./../imgs/maxresdefault.jpg" />
+  </div>
+</a>
+`
+}
