614a9a852c6c322cf8aef9c5a76dc599e8db292f
dpclfk <lee@iseungbaeui-MacBookAir.local>
2024-06-03 14:35:23 +0900

    try 오류 수정

--- backEnd/services/board/category.js
+++ backEnd/services/board/category.js
@@ -1,25 +1,25 @@
 import { Channel, Category } from "../../models/index.js";
 
 export default async (req, res) => {
-  const reqbody = req.body;
-  let chname = reqbody.channel;
-
-  if (!chname) {
-    chname = "main";
-  }
-
-  const channel = await Channel.findOne({
-    where: { engTitle: chname },
-    attributes: ["id"],
-  });
-
-  const category = await Category.findAll({
-    where: { channelId: channel.id },
-    attributes: ["engTitle", "name"],
-  });
-
-  res.json({ category: category });
   try {
+    const reqbody = req.body;
+    let chname = reqbody.channel;
+
+    if (!chname) {
+      chname = "main";
+    }
+
+    const channel = await Channel.findOne({
+      where: { engTitle: chname },
+      attributes: ["id"],
+    });
+
+    const category = await Category.findAll({
+      where: { channelId: channel.id },
+      attributes: ["engTitle", "name"],
+    });
+
+    res.json({ category: category });
   } catch (err) {
     console.error(err);
     res.status(419);
