6f14ec7ebba2d5d78acfecc7d0016fed1bc33870
dpclfk <lee@iseungbaeui-MacBookAir.local>
2024-06-04 10:47:22 +0900

    try오류 수정

--- backEnd/services/board/category.js
+++ backEnd/services/board/category.js
@@ -1,19 +1,19 @@
 import { Channel, Category } from "../../models/index.js";
 
 export default async (req, res) => {
-  const reqbody = req.body;
-  const chname = reqbody.channel;
-
-  const channel = await Channel.findOne({
-    where: { engTitle: chname },
-  });
-
-  const category = await Category.findAll({
-    where: { channelId: channel.id },
-  });
-
-  res.json({ category: category });
   try {
+    const reqbody = req.body;
+    const chname = reqbody.channel;
+
+    const channel = await Channel.findOne({
+      where: { engTitle: chname },
+    });
+
+    const category = await Category.findAll({
+      where: { channelId: channel.id },
+    });
+
+    res.json({ category: category });
   } catch (err) {
     console.error(err);
     res.status(419);
--- backEnd/services/board/modifycheck.js
+++ backEnd/services/board/modifycheck.js
@@ -1,42 +1,42 @@
 import { Channel, ChannelAdmin, Board, Category } from "../../models/index.js";
 
 export default async (req, res) => {
-  const nowuser = req.user;
-  if (!nowuser) {
-    throw new Error("not logged in");
-  }
-
-  const reqbody = req.body;
-  const chname = reqbody.channel;
-  const catename = reqbody.category;
-
-  const reqcuery = req.query;
-  const nowview = reqcuery.boardId;
-  const channel = await Channel.findOne({
-    where: { engTitle: chname },
-    include: [{ model: ChannelAdmin }],
-  });
-
-  const board = await Board.findOne({
-    where: { id: nowview, userId: nowuser.id },
-    include: {
-      model: Category,
-    },
-  });
-  const channeladmin = await ChannelAdmin.findAll({
-    where: { channelId: channel.id, userId: nowuser.id },
-  });
-  if (!board || !channeladmin) {
-    throw new Error("not match user");
-  }
-
-  res.json({
-    // user: nowuser,
-    channel: channel,
-    // channemAdmin: channemAdmin,
-    board: board,
-  });
   try {
+    const nowuser = req.user;
+    if (!nowuser) {
+      throw new Error("not logged in");
+    }
+
+    const reqbody = req.body;
+    const chname = reqbody.channel;
+    const catename = reqbody.category;
+
+    const reqcuery = req.query;
+    const nowview = reqcuery.boardId;
+    const channel = await Channel.findOne({
+      where: { engTitle: chname },
+      include: [{ model: ChannelAdmin }],
+    });
+
+    const board = await Board.findOne({
+      where: { id: nowview, userId: nowuser.id },
+      include: {
+        model: Category,
+      },
+    });
+    const channeladmin = await ChannelAdmin.findAll({
+      where: { channelId: channel.id, userId: nowuser.id },
+    });
+    if (!board || !channeladmin) {
+      throw new Error("not match user");
+    }
+
+    res.json({
+      // user: nowuser,
+      channel: channel,
+      // channemAdmin: channemAdmin,
+      board: board,
+    });
   } catch (err) {
     console.error(err);
     if (err.message == "not logged in") {
